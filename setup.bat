94d60d04d68b82e208ed76daec821332afd554a1
brett-haas <59572111+brett-haas@users.noreply.github.com>
2024-11-12 15:55:57 -0500

    Delete setup.bat

--- setup.bat
+++
@@ -1,6 +0,0 @@
-if (-not (npm list ansi-escapes > $null 2>&1)) {
-    Write-Output "Installing required modules..."
-    npm install ansi-escapes > $null 2>&1
-} else {
-    Write-Output "Required modules already installed..."
-}
