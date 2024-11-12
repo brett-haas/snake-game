9ecdde36785684c5538d7d1b0a8a1f1f35af83d6
brett-haas <59572111+brett-haas@users.noreply.github.com>
2024-11-12 15:34:59 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,9 +1,6 @@
-@echo off
-
-npm list ansi-escapes >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Installing required modules...
-    npm install ansi-escapes >nul 2>&1
-) else (
-    echo Required modules already installed...
-)
+if (-not (npm list ansi-escapes > $null 2>&1)) {
+    Write-Output "Installing required modules..."
+    npm install ansi-escapes > $null 2>&1
+} else {
+    Write-Output "Required modules already installed..."
+}
